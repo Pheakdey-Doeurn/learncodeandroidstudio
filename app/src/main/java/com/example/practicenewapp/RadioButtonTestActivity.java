4e515c7093eb6389f393703246b1086da97a93c3
Pheakdey Doeurn <pheakdey.96kh@gmail.com>
2024-01-05 17:12:17 +0700

    New Update

--- app/src/main/java/com/example/practicenewapp/RadioButtonTestActivity.java
+++ app/src/main/java/com/example/practicenewapp/RadioButtonTestActivity.java
@@ -1,53 +1,47 @@
 package com.example.practicenewapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.CheckBox;
 import android.widget.ImageView;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.RadioButton;
 
 public class RadioButtonTestActivity extends AppCompatActivity {
     ImageView imgNopic;
+    RadioGroup rgFruit;
     RadioButton rbFruit;
-    RadioGroup rgGroup;
-    TextView txtFruit;
-    @SuppressLint("MissingInflatedId")
+    TextView txtResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radio_button_test);
 
-        rgGroup=findViewById(R.id.rgGroup);
-        txtFruit= findViewById(R.id.txtFruit);
-        imgNopic =findViewById(R.id.imgNopic);
+        rgFruit = findViewById(R.id.rgFruit);
+        txtResult = findViewById(R.id.txtResult);
+        imgNopic = findViewById(R.id.imgNopic);
 
-        rgGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        rgFruit.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup rgGroup, int i) {
-               if(i == R.id.rbMango) {
-                   imgNopic.setImageResource(R.drawable.mang);
-                   txtFruit.setText("Mango");
-               }else if(i == R.id.rbOrange) {
-                       imgNopic.setImageResource(R.drawable.oran);
-                       txtFruit.setText("Orange");
-               }else if(i == R.id.rbBanana) {
-                        imgNopic.setImageResource(R.drawable.bana);
-                        txtFruit.setText("Banana");
-               }else if(i == R.id.rbGrape) {
-                        imgNopic.setImageResource(R.drawable.grape);
-                        txtFruit.setText("Grape");
-               }
+                if (i == R.id.rbMango) {
+                    imgNopic.setImageResource(R.drawable.mang);
+                    txtResult.setText("Mango");
+                } else if (i == R.id.rbOrange) {
+                    imgNopic.setImageResource(R.drawable.oran);
+                    txtResult.setText("Orange");
+                } else if (i == R.id.rbBanana) {
+                    imgNopic.setImageResource(R.drawable.bana);
+                    txtResult.setText("Banana");
+                } else if (i == R.id.rbGrape) {
+                    imgNopic.setImageResource(R.drawable.grape);
+                    txtResult.setText("Grape");
+                }
+                rbFruit=findViewById(i);
+                txtResult.setText(rbFruit.getText().toString());
 
-                rbFruit= findViewById(i);
-                txtFruit.setText(rbFruit.getText().toString());
             }
         });
-
     }
-
 }
