65a30d2ee0ed70a58ccc3d4a731de2c23ae6b7a9
Pheakdey Doeurn <pheakdey.96kh@gmail.com>
2024-01-08 08:37:58 +0700

    Update New

--- app/src/main/java/com/example/practicenewapp/IntentMultiActivity.java
+++ app/src/main/java/com/example/practicenewapp/IntentMultiActivity.java
@@ -6,20 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class IntentMultiActivity extends AppCompatActivity {
 
     Button btnClick;
+    EditText editUsername,editPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_multi);
+
+        editUsername = findViewById(R.id.txtUsername);
+        editPass = findViewById(R.id.txtPass);
 
         btnClick = findViewById(R.id.btnNext);
         btnClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(IntentMultiActivity.this,OneIntentMultiActivity.class);
+                i.putExtra("KeyUname",editUsername.getText().toString().trim());
+                i.putExtra("KeyUpass",editPass.getText().toString().trim());
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/practicenewapp/OneIntentMultiActivity.java
+++ app/src/main/java/com/example/practicenewapp/OneIntentMultiActivity.java
@@ -6,14 +6,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class OneIntentMultiActivity extends AppCompatActivity {
 
     Button btnPre;
+    TextView txtUsername,txtPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_intent_multi);
+
+        //get value from Intent Class
+        Intent getValue = getIntent();
+        String strName,strPass;
+
+        strName=getValue.getStringExtra("KeyUname");
+        strPass=getValue.getStringExtra("KeyUpass");
+
+        txtUsername.setText(strName);
+        txtPass.setText(strPass);
+
+        //Find Value in TextView
+        txtUsername = findViewById(R.id.txtUsername);
+        txtPass = findViewById(R.id.txtPass);
+
+        if(strName.equals("User001")&& strPass.equals("User@001")){
+            txtUsername.setText(strName);
+            txtPass.setText(strPass);
+        }else{
+            Toast.makeText(this, "User not Found", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(OneIntentMultiActivity.this,IntentMultiActivity.class);
+            startActivity(i);
+        }
         btnPre =  findViewById(R.id.btnPre);
         btnPre.setOnClickListener(new View.OnClickListener() {
             @Override
